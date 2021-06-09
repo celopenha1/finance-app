96740c4f9e2ebc49d6c91f3005e40b9e798535cc
Marcelo Penha <niqcelo@gmail.com>
2021-06-08 21:32:46 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,8 @@ import Table from './components/Table/Table';
 
 
 function App() {
-
-
   return (
-
-    <div>
-      <Container />
-    </div>
-
+    <Container />
   );
 
 }
@@ -22,26 +16,39 @@ const Container = (props) => {
   const [title, setTitle] = useState("");
   const [value, setValue] = useState("");
 
-  const handleSubmit = event => {
-
+  const handleSubmit = async event => {
     event.preventDefault();
 
     const data = {
       title,
       value
     }
-    // "https://fnance-app.herokuapp.com/transaction"
-    fetch("http://localhost:3001/transaction", {
+    const url = "https://fnance-app.herokuapp.com/transaction";
+    const requestConfig = {
       method: "POST",
       headers: {
         'Content-type': 'application/json; charset=UTF-8'
       },
       body: JSON.stringify(data)
-    }).then(response => { if (response.ok) { document.querySelector('button').textContent = "ENVIADO COM SUCESSO" } return response.json() })
-      .then(data => console.log(data))
-      .catch(error => { document.querySelector('button').style.background = "red"; document.querySelector('button').textContent = "NÃO FOI POSSÍVEL SALVAR"; console.log(error) });
+    }
+    function handleSuccessSubmit() {
+      document.querySelector('button').textContent = "ENVIADO COM SUCESSO"
+    }
+    function handleFailedSubmit(error) {
+      document.querySelector('button').style.background = "red";
+      document.querySelector('button').textContent = "FALHA NO ENVIO DA MENSAGEM";
+      console.log(error);
+    }
+
+    try {
+      const response = await fetch(url, requestConfig);
+      if (response.ok) handleSuccessSubmit();
+    } catch (error) {
+      handleFailedSubmit(error)
+    }
 
   }
+
 
   return (
     <div className="container">
@@ -64,8 +71,6 @@ const Container = (props) => {
     </div>
   )
 }
-
-
 
 
 export default App
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+
+  entry: ['babel-polyfill', './src/index.js'],
+
   // Where files should be sent once they are bundled
   output: {
     path: path.join(__dirname, '/dist'),
@@ -28,5 +31,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+  plugins: [new HtmlWebpackPlugin({ template: './index.html' }), ]
 }
